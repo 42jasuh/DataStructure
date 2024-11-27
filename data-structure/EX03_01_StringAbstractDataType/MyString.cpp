7ab7bc453cd9c083dd0b122014d31cc53a9c7005
42jasuh <diqkqkqk@yahoo.com>
2024-11-27 12:15:01 +0900

    파일 수정

--- data-structure/EX03_01_StringAbstractDataType/MyString.cpp
+++ data-structure/EX03_01_StringAbstractDataType/MyString.cpp
@@ -13,34 +13,24 @@ MyString::MyString()
 MyString::MyString(const char* init)
 {
 	// 크기(size_) 결정
-    int i = 0;
-	while(init[i]!='\0')
-		size_++;
-	str_ = new char[size_];
 
 	// 메모리 할당
-    str_ = new char[size_];
 
 	// 데이터 복사
-	memcpy(str_, init, size_);
 }
 
 // MyString의 다른 instance로부터 초기화
 MyString::MyString(const MyString& str)
-{    
-	// 기본 복사 생성자는 포인터 주소를 복사하기 때문에 소멸시 오류 발생
-	// 여기서는 새로운 메모리를 할당 받아 복사
-    size_ = str.size_;
-	str_ = new char[size_];
-	memcpy(str_, str.str_, size_);
+{
+	// 기본 복사 생성자는 포인터 주소만 복사하기 때문에 
+	// 소멸시 오류 발생
+	// 여기서는 새로 메모리를 할당 받아서 복사
+
 }
 
 MyString::~MyString()
 {
-	if (str_ != nullptr)
-	{
-		delete str_;
-	}
+	// 메모리 해제
 }
 
 bool MyString::IsEmpty()
@@ -50,12 +40,8 @@ bool MyString::IsEmpty()
 
 bool MyString::IsEqual(const MyString& str) // 편의상 참조& 사용
 {
-	if (this->size_ != str.size_)
-		return false;
-	
-	for (int i = 0; i < this->size_; i++)
-		if (str_[i] != str.str_[i])
-			return false;
+	// 힌트: str.str_, str.size_ 가능
+
 	return false;
 }
 
@@ -67,29 +53,17 @@ int MyString::Length()
 void MyString::Resize(int new_size)
 {
 	// 메모리 재할당과 원래 갖고 있던 내용 복사
-	if (new_size != size_)
-	{
-		char *new_str = new char[new_size];
-		for (int i = 0; i < (new_size < size_ ? new_size : size_); i++)
-			new_str[i] = str_[i];
-
-		delete[] str_;
-		str_ = new_str;
-		size_ = new_size;
-	}
 }
 
 // 인덱스 start위치의 글자부터 num개의 글자로 새로운 문자열 만들기
 MyString MyString::Substr(int start, int num)
 {
 	// 복사할 인덱스: start, start + 1, ... , start + num - 1
-	assert(start + num - 1 < this->size_); // 문제를 단순하게 만들기 위해 가정
+	// assert(start + num - 1 < this->size_); // 문제를 단순하게 만들기 위해 가정
 
 	MyString temp;
-	temp.Resize(num);
 
-	for(int i = 0; i < num; i++)
-		temp.str_[i] = this->str_[start+i];
+	// TODO:
 
 	return temp;
 }
@@ -98,11 +72,7 @@ MyString MyString::Concat(MyString app_str)
 {
 	MyString temp;
 
-	temp.Resize(this->size_ + app_str.size_);
-
-	memcpy(temp.str_, this->str_, this->size_);
-	memcpy(&temp.str_[size_], app_str.str_, app_str.size_);
-
+	// TODO: 
 
 	return temp;
 }
@@ -114,37 +84,20 @@ MyString MyString::Insert(MyString t, int start)
 
 	MyString temp;
 
-	temp.Resize(size_+t.size_);
-	for (int i = 0; i < start; i++)
-		temp.str_[i] = str_[i];
-	
-	for (int i = start; i < start + t.size_; i++)
-		temp.str_[start] = t.str_[i-start];
-	
-	for (int i = start+size_; i < size_+t.size_; i++)
-		temp.str_[i] = str_[i-t.size_];
+	// TODO:
 
 	return temp;
-} 
+}
 
 int MyString::Find(MyString pat)
 {
-	for (int start = 0; start <= Length() - pat.Length(); start++)
-	{
-		for (int j = 0; j < pat.Length(); j++)
-		{
-			if (str_[start+j] != pat.str_[j])
-				break;
-			if (j == pat.Length() - 1)
-				return start;
-		}
-	}
+	//TODO:
 
 	return -1;
 }
 
 void MyString::Print()
-{	
+{
 	for (int i = 0; i < size_; i++)
 		cout << str_[i];
 	cout << endl;
